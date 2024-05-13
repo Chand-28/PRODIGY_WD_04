cd48f66f56b9b9610e7daecf9aacb528353f68c4
Chand-28 <168813134+Chand-28@users.noreply.github.com>
2024-05-13 17:33:27 +0530

    Add files via upload

---
+++ g.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll("nav ul li a");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            const targetId = this.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                const offsetTop = targetSection.offsetTop;
+                window.scroll({
+                    top: offsetTop,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
